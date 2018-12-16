c64b720d38de26497b27f69c0ba93d3466b1c08b
bruno1022 <40252090+bruno1022@users.noreply.github.com>
2018-12-16 19:56:23 -0200

    first commit

---
+++ src/test/java/com/javaee/bruno/mercadoacoes/MercadoAcoesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.javaee.bruno.mercadoacoes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MercadoAcoesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
